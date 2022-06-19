ec7add55ced87b80728de612c06befef8abd6e1d
Ruchiket Borse <RuchiketBorse1@gmail.com>
2022-06-19 20:13:57 +0530

    home container created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+// components
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Ruchiket</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const home = ()=>{
+    return(
+        <div className="home-container">
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default home;
